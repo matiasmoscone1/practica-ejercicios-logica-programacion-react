dbcc49696dee16f5ff59ec0c51ad6dddeac85f6c
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-08-07 20:06:13 -0300

    Ejercicio 1

--- src/ejercicio-1/Contador.jsx
+++ src/ejercicio-1/Contador.jsx
@@ -1,6 +1,37 @@
+import { useState } from "react";
+
+const Contador = () => {
+
+    const [contador, setContador] = useState(0);
+
+
+    const sumarFunc = () => {
+        contador ++;
+    };
+
+    const restarFunc = () => {
+        contador --;
+    }
+
+
+    return(
+        <>
+        <div>
+            <h2>Contador</h2>
+            <button onClick={sumarFunc}>Agregar</button>
+            <button onClick={restarFunc}>Restar</button>
+            <br/>
+            <span>Resultado: </span>
+        </div>
+        </>
+    )
 
 
 
+}
+
+
+export default Contador;
 
 
 

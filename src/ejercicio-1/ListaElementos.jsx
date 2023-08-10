0fb617f8e2a425762c08f549b94a811cfc8e70e5
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-08-10 19:18:32 -0300

    Documentando ej 2!

--- src/ejercicio-1/ListaElementos.jsx
+++ src/ejercicio-1/ListaElementos.jsx
@@ -3,10 +3,12 @@
 //2. Lista de elementos: Crea un componente que muestre una lista de nombres. 
 //Utiliza un arreglo de nombres y recórrelo para mostrar cada nombre en la lista.
 
-import { useState } from "react";
+//import { useState } from "react";
 
+//Creando arreglo de nombres
 const arregloNombres = ["Matias", "Marco", "Agustin", "Joaquin", "Lautaro"];
 
+//Creando componente 
 const ListaElementos = () => {
 
     //const [nombres, setNombres] = useState(arregloNombres);
@@ -15,7 +17,8 @@ const ListaElementos = () => {
         <>
         <div>
             <h2>Ejercicio 2: Lista de elementos</h2>
-            {
+            {//Recorre el arreglo de nombres creado y muestra en el Dom cada nombre con su
+            //respectiva key
                 arregloNombres.map((nombre) => {
                     return(
                     <ul key={nombre}>

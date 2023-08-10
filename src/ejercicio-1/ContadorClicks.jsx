a9741b5351a96479f02588fd2c277656971b5b5f
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-08-10 19:42:56 -0300

    Documentando ej 3

--- src/ejercicio-1/ContadorClicks.jsx
+++ src/ejercicio-1/ContadorClicks.jsx
@@ -6,13 +6,15 @@
 
 import { useState } from "react";
 
+//Creando componente
 const ContadorClicks = () => {
 
+    //Creando estados iniciales para cada boton
     const [boton1, setBoton1] = useState(0);
     const [boton2, setBoton2] = useState(0);
     const [boton3, setBoton3] = useState(0);
 
-    
+    //Creando funciones respectivas para cada boton
     const clickBoton1 = () => {
         setBoton1(boton1 + 1);
     }
@@ -29,6 +31,8 @@ const ContadorClicks = () => {
         <div>
             <h2>Ejercicio 3: Contador clicks</h2>
             <br/>
+            {/* Renderizando los botones y actualizandolos cada vez q le hacen
+            //click, llama a la funcion respectiva de cada boton*/}
             <button onClick={clickBoton1}>boton1: {boton1}</button>
             <button onClick={clickBoton2}>boton2: {boton2}</button>
             <button onClick={clickBoton3}>boton3: {boton3}</button>
@@ -40,6 +44,7 @@ const ContadorClicks = () => {
 
 }
 
+//Exporta por defecto el componente
 export default ContadorClicks;
 
 

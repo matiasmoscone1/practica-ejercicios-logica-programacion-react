af79a3b84c40bf87fed90c06854789f711e4ce3b
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-08-12 19:31:32 -0300

    Resolviendo ej 5

--- src/ejercicio-1/CalculadoraSimple.jsx
+++ src/ejercicio-1/CalculadoraSimple.jsx
@@ -6,7 +6,26 @@ import { useState } from "react";
 
 const CalculadoraSimple = () => {
 
+    const [primerNum, setPrimerNum] = useState(0);
+    const [segundoNum, setSegundoNum] = useState(0);
+    let resultado = 0;
 
+    console.log(primerNum);
+    console.log(segundoNum);
+
+    resultado = parseInt(primerNum) + parseInt(segundoNum);
+
+    console.log(resultado);
+    return(
+    <>
+        <div>
+            <h2>Ejercicio 5: Calculadora simple</h2>
+            <button>Sumar</button><br/>
+            <input type="number" onChange={(e) => {setPrimerNum(e.target.value)}}/>
+            <input type="number" onChange={(e) => {setSegundoNum(e.target.value)}}/>
+            <p>{}</p>
+        </div>
+    </>)
 
 }
 

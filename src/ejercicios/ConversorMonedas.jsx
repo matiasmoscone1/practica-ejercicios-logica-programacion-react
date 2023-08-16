57106895ee0187fe76169edbe998d8e184880ddf
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-08-16 17:32:08 -0300

    Documentando ej 6

--- src/ejercicios/ConversorMonedas.jsx
+++ src/ejercicios/ConversorMonedas.jsx
@@ -3,19 +3,21 @@
 //6. Conversor de Moneda: Crea un componente de React que convierta una cantidad en una 
 //moneda a otra moneda, utilizando tasas de cambio fijas.
 
+//Importando hook useState
 import { useState } from "react";
 
+//Creando componente 
 const ConversorMonedas = () => {
 
-    
+    //Creando variables de estado
     const [dolares, setDolares] = useState("");
     const [pesos, setPesos] = useState("");
 
-
+    //funcion que toma el estado actual del input del dolar y setea los pesos haciendo el calculo
     const convertirDolares = (dol) => {
         setPesos(dol * 750);
     }
-
+    //funcion que toma el estado actual del input del peso y setea los dolares haciendo el calculo
     const convertirPesos = (pes) => {
         setDolares(pes / 750);
     }
@@ -26,6 +28,10 @@ const ConversorMonedas = () => {
         <h2>6. Conversor de monedas</h2>
         <br/>
         <label>USD </label>
+        {/* Cada input refleja su valor de acuerdo a la variable de estado, si cambia ese valor
+        actualiza automaticamente por el valor nuevo y llama a dicha funcion para que haga la
+        conversion automatica de la moneda
+        */}
         <input value={dolares} onChange={(e) => {
             const nuevosDolares = e.target.value;
             setDolares(nuevosDolares);

92e5c0f8655266d8fd1df8014993478d32bb5ae7
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-08-16 16:51:02 -0300

    Resolviendo ej 6

--- src/ejercicios/ConversorMonedas.jsx
+++ src/ejercicios/ConversorMonedas.jsx
@@ -7,6 +7,28 @@ import { useState } from "react";
 
 const ConversorMonedas = () => {
 
+    const [valor, setValor] = useState(0);
+
+
+    const convertirDolares = () => {
+
+    }
+
+    const convertirPesos = () => {
+        
+    }
+
+
+    return(<>
+    <div>
+        <h2>6. Conversor de monedas</h2>
+        <br/>
+        <label>USD</label>
+        <input />
+        <label> ARS</label>
+        <input />
+    </div>
+    </>)
 
 
 }

ae570090e19d2d7a4af1b44bff36297ae6f41b70
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-08-22 20:00:01 -0300

    Resolviendo ej 7

--- src/ejercicios/MostrarHora.jsx
+++ src/ejercicios/MostrarHora.jsx
@@ -8,13 +8,30 @@ import { useState } from "react";
 
 const MostrarHora = () => {
 
+    const [hora, setHora] = useState("");
+
+    
+    
+
+    const intervalo = setInterval(() => {
+        const fechaHoy = new Date;
+        console.log(fechaHoy);
+
+
+    }, 1000);
+
+
+    const cerrarIntervalo = () => {
+        clearInterval(intervalo);
+    }
 
     
     return(<>
     <div>
         <h2>7. Mostrar hora en tiempo real</h2>
-        
-
+        <button>Mostrar</button>
+        <button onClick={() => {cerrarIntervalo()}}>Ocultar</button>
+        <span></span>
 
     </div>
     </>)

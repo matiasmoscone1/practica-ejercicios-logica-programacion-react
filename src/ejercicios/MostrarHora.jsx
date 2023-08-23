ff0c94ab2521717b38321e81dadb82e740388f21
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-08-23 19:17:14 -0300

    Resolviendo logica del problema 7

--- src/ejercicios/MostrarHora.jsx
+++ src/ejercicios/MostrarHora.jsx
@@ -3,40 +3,58 @@
 //Actualiza la hora cada segundo.
 
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 
 const MostrarHora = () => {
 
     
-    let intervalo;
-    let hora, minutos, segundos;
+    const [intervalo, setIntervalo] = useState(null);;
+    const [hora, setHora] = useState("");
+    const [minutos, setMinutos] = useState("");
+    const [segundos, setSegundos] = useState("");
+
+    //let hora, minutos, segundos;
     
     const crearIntervalo = () => {
-        intervalo = setInterval(() => {
+        const nuevoIntervalo = setInterval(() => {
             const fechaHoy = new Date;
             
-            let hora = fechaHoy.getHours();
-            let minutos = fechaHoy.getMinutes();
-            let segundos = fechaHoy.getSeconds();
+            //let hora = fechaHoy.getHours();
+            //let minutos = fechaHoy.getMinutes();
+            //let segundos = fechaHoy.getSeconds();
+            let nuevaHora = setHora(fechaHoy.getHours());
+            let nuevoMinuto = setMinutos(fechaHoy.getMinutes());
+            let nuevoSegundo = setSegundos(fechaHoy.getSeconds());
             
             console.log(fechaHoy);
-            console.log(hora,minutos,segundos);
+            console.log(nuevaHora,nuevoMinuto,nuevoSegundo);
+            
 
         }, 1000);
+
+        setIntervalo(nuevoIntervalo);
+
     }
 
     const cerrarIntervalo = () => {
         clearInterval(intervalo);
     }
 
-    
+    useEffect(() => {
+        return() => {
+            if(intervalo){
+                clearInterval(intervalo);
+            }
+        }
+    },[intervalo]);
+
     return(<>
     <div>
         <h2>7. Mostrar hora en tiempo real</h2>
         <button onClick={() => {crearIntervalo()}}>Mostrar</button>
-        <button onClick={() => {cerrarIntervalo()}}>Ocultar</button>
-        <span>{`${hora}:${minutos}:${segundos}`}</span>
+        <button onClick={() => {cerrarIntervalo()}}>Ocultar</button>        
+        <span> </span>
 
     </div>
     </>)

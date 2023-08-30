f56f656a2bd9529d2dde68d51370b74ed0da01f9
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-08-30 19:31:17 -0300

    Maquetando ej 8

--- src/ejercicios/VisualizarImagen.jsx
+++ src/ejercicios/VisualizarImagen.jsx
@@ -2,15 +2,23 @@
 
 import { useState } from "react";
 
+
+//8. Visualizador de Imágenes: Crea un componente de React que muestre una lista de 
+//imágenes en miniatura. Al hacer clic en una imagen, se debe mostrar la imagen en 
+//tamaño completo.
+
+
 const VisualizarImagen = () => {
 
 
 
     return(<>
         <div>
+            <h2>8. Visualizador de imagenes</h2>
+            <div>
+            <img src="imagenes/img1.jpg"/>
 
-        
-
+            </div>
             
         </div>   
     </>)

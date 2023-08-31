5b3a642f69bbd32e3685064b35d0a0a74dd09afe
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-08-31 18:26:18 -0300

    Resolviendo ej 9

--- src/ejercicios/Temporizador.jsx
+++ src/ejercicios/Temporizador.jsx
@@ -1,14 +1,37 @@
 
-
 //9. Temporizador: Crea un componente de React que muestre un temporizador que 
 //pueda ser configurado para contar hacia abajo desde un valor dado.
+
+import { useState } from "react";
+
 
 
 const Temporizador = () => {
 
+    const [tiempo, setTiempo] = useState(null);
 
 
+    const actualizarIntervalo = (valor) => {
+        let segundos;
+        setInterval(() => {
+            segundos = valor - 1;
+            console.log(segundos);
+        }, 1000);
 
+    }
+
+    return(<>
+    <div>
+        <h2>9. Temporizador regresivo</h2>
+
+        <br/>
+        <input placeholder="Escribe los segundos" onChange={(e) => setTiempo(e.target.value)}/>
+        <button onClick={() => {actualizarIntervalo(tiempo)}}>Empezar</button>
+        <button>Ocultar</button>
+
+
+    </div>
+    </>)
 
 }
 

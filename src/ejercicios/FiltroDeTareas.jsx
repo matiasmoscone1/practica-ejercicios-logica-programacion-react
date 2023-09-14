e4d72a1ac9f28e49e91490262c702bbd14906cb6
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-09-14 19:17:11 -0300

    Agregando ejercicio 11

---
+++ src/ejercicios/FiltroDeTareas.jsx
@@ -0,0 +1,62 @@
+
+//11. Filtro de Lista de Tareas: Crea una lista de tareas con un filtro que permita 
+//a los usuarios mostrar solo tareas completadas, incompletas o todas.
+
+const FiltroDeTareas = () => {
+
+    const tareas = [{
+        actividad: "Tarea 1",
+        check: true
+    },
+    {
+        actividad: "Tarea 2",
+        check: true
+    },
+    {
+        actividad: "Tarea 3",
+        check: false
+    },
+    {
+        actividad: "Tarea 4",
+        check: true
+    },
+    {
+        actividad: "Tarea 5",
+        check: false
+    },
+    {
+        actividad: "Tarea 6",
+        check: false
+    },
+    {
+        actividad: "Tarea 7",
+        check: true
+    },
+    {
+        actividad: "Tarea 8",
+        check: true
+    },
+    {
+        actividad: "Tarea 9",
+        check: false
+    }
+    ];
+
+
+
+
+
+    return(<>
+    <div>
+        <h2>10. Filtro lista de tareas</h2>
+        
+
+
+    </div>
+    </>)
+
+
+
+}
+
+

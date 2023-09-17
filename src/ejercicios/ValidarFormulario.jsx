59377a811fdda9888afd65dfcec50e18e52369fb
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-09-17 17:12:52 -0300

    Documentando ej 12

--- src/ejercicios/ValidarFormulario.jsx
+++ src/ejercicios/ValidarFormulario.jsx
@@ -5,41 +5,50 @@ import { useState } from "react";
 //nombre, correo electrónico y contraseña. Agrega validaciones para asegurarte 
 //de que los datos ingresados sean válidos antes de enviar el formulario.
 
-
+//creando componente
 const ValidarFormulario = () => {
 
+    //creando variables de estado que almacenara el estado actual de cada variable
     const [usuario, setUsuario] = useState("");
     const [contrasena, setContrasena] = useState("");
     const [email, setEmail] = useState("");
 
+    //creando expresiones regulares para hacer validaciones
     const validaUsuario = /^[a-zA-Z0-9]{8,15}$/;
     const validaContra = /^[a-zA-Z0-9\s!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]{8,15}$/
     const validaEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
 
-
+    //creando funcion que valida el usuario
     const checkUsuario = (usu) => {
+        //se valida la expresion regular con la variable usuario que se le pasa por parametro 
         validaUsuario.test(usu) ? document.querySelector(".resultado-usuario").textContent = " Usuario validado con exito!!!"
                                     //console.log("Usuario validado con exito!!!")
                                 : document.querySelector(".resultado-usuario").textContent = " Usuario no validado... (debe contener letras o numeros y un ragno entre 8 y 15 caracteres";
                                     //console.log("Usuario no validado... (debe contener letras o numeros y un ragno entre 8 y 15 caracteres");   
     }
 
+    //creando funcion que valida la contraseña
     const checkContra = (con) => {
+         //se valida la expresion regular con la variable contraseña que se le pasa por parametro 
         validaContra.test(con) ? document.querySelector(".resultado-contrasena").textContent = " Contraseña validada con exito!!!" 
                                 //console.log("Contraseña validada con exito!!!")
                                :  document.querySelector(".resultado-contrasena").textContent = " Contraseña no validada... (debe contener letras, numeros o simbolos y un ragno entre 8 y 15 caracteres";
                                 //console.log("Contraseña no validada... (debe contener letras, numeros o simbolos y un ragno entre 8 y 15 caracteres");   
     }
 
+    //creando funcion que valida el email
     const checkEmail = (ema) => {
+         //se valida la expresion regular con la variable email que se le pasa por parametro 
         validaEmail.test(ema) ? document.querySelector(".resultado-email").textContent = " Email validado con exito!!!"
                                 //console.log("Email validado con exito!!!")
                               : document.querySelector(".resultado-email").textContent = " Email no validado... (debe tener formato asd@asd.asd";
                                 //console.log("Email no validado... (debe tener formato asd@asd.asd"); 
        
     }
-
+    
+    //funcion que al clickea el boton dentro del form, llama a las 3 funciones especificas para validar
     const validarCompleto = (e) => {
+        //previene el comportamiento por default del submit para que no envie nada
         e.preventDefault();
         checkUsuario(usuario); 
         checkContra(contrasena);   
@@ -50,6 +59,9 @@ const ValidarFormulario = () => {
     <div>
         <h2>12. Validar formulario </h2>
         <form>
+            {/* Form completo con inputs que a medida que se van escribiendo, van seteando la variable
+            correspondiente para luego ser utilizada. Se crea la etiqueta span y se injecta javascript puro
+            para mostrar el resultado en el DOM*/}
             <button onClick={(e) => validarCompleto(e)}>Enviar</button><br/><br/>
             <label>Usuario </label>
             <input onChange={(e) => setUsuario(e.target.value)}/>
@@ -66,6 +78,7 @@ const ValidarFormulario = () => {
 
 }
 
+//exporta por default el componente ValidarFormulario
 export default ValidarFormulario;
 
 

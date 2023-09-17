1d0a75cd53d41195d20e7ebb8d599c082e751f33
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-09-17 17:26:01 -0300

    Resolviendo ejercicio 13

--- src/ejercicios/OrdenarDatos.jsx
+++ src/ejercicios/OrdenarDatos.jsx
@@ -6,12 +6,44 @@
 
 const OrdenarDatos = () => {
 
+    const datos = [{
+        nombre: "Matias",
+        edad: 27,
+        fecha: "16/05/1996"
+    },
+    {
+        nombre: "Messi",
+        edad: 36,
+        fecha: "24/06/1987"
+    },
+    {
+        nombre: "Dibu",
+        edad: 31,
+        fecha: "02/09/1992"
+    },
+    {
+        nombre: "Cuti",
+        edad: 25,
+        fecha: "27/04/1998"
+    }];
+
+
+
+
 
     return(<>
     <div>
         <h2>13. Ordenamiento de datos </h2>
+        <button>Nombre</button>{" "}
+        <button>Edad</button>{" "}
+        <button>Fecha</button><br/><br/>
+        <div>
+            {datos.map((jugador) => {
+                return(<li key={jugador.nombre}>{` Nombre: ${jugador.nombre} Edad: ${jugador.edad} Fecha: ${jugador.fecha}`}</li>)
+            })}
+        </div>
 
-        
+
     </div>
     </>)
 

b7d9bfe9b8280c14ba32fb9b5d8d34cb7edac2b6
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-09-26 19:46:30 -0300

    Resolviendo ejercicio 14

--- src/ejercicios/TarjetaInteractiva.jsx
+++ src/ejercicios/TarjetaInteractiva.jsx
@@ -12,7 +12,14 @@ const TarjetaInteractiva = () => {
     return(<>
     <div>
         <h2>14. Tarjeta Interactiva </h2>
-        
+        <p style={{color:"red"}}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vitae quaerat aperiam
+        corrupti, beatae ad esse! Atque ea ab sapiente nemo aliquam assumenda, ut esse alias? 
+        Nemo cupiditate numquam aspernatur ipsum!</p>
+
+        <p style={{color:"green"}}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vitae quaerat aperiam
+        corrupti, beatae ad esse! Atque ea ab sapiente nemo aliquam assumenda, ut esse alias? 
+        Nemo cupiditate numquam aspernatur ipsum!</p>
+
 
     </div>    
     </>)
@@ -21,7 +28,7 @@ const TarjetaInteractiva = () => {
 }
 
 
-export default TarjetaInteractiva();
+export default TarjetaInteractiva;
 
 
 

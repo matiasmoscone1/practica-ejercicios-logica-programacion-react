1ff3e1af2ab73527042476b66ef157c9ef244951
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-09-27 19:21:18 -0300

    Resolviendo ejercicio 14

--- src/ejercicios/TarjetaInteractiva.jsx
+++ src/ejercicios/TarjetaInteractiva.jsx
@@ -3,11 +3,14 @@
 //al hacer clic en ella. Debe mostrar información en el frente y en la parte posterior 
 //de la tarjeta.
 
+import { useState } from "react";
 
 const TarjetaInteractiva = () => {
 
 
+    const cambiarTarjeta = () => {
 
+    }
 
     return(<>
     <div>
@@ -15,12 +18,13 @@ const TarjetaInteractiva = () => {
         <p style={{color:"red"}}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vitae quaerat aperiam
         corrupti, beatae ad esse! Atque ea ab sapiente nemo aliquam assumenda, ut esse alias? 
         Nemo cupiditate numquam aspernatur ipsum!</p>
+        <br/>
+        <button >Toggle</button>
 
-        <p style={{color:"green"}}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vitae quaerat aperiam
-        corrupti, beatae ad esse! Atque ea ab sapiente nemo aliquam assumenda, ut esse alias? 
-        Nemo cupiditate numquam aspernatur ipsum!</p>
-
-
+        <p style={{color:"green"}}>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+        aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+        aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
+        
     </div>    
     </>)
 

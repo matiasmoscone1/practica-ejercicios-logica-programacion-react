9f08fe4316b42d4679f23cd4ebeb56f624ed9e16
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-09-27 19:36:18 -0300

    Ejercicio 14 documentado

--- src/ejercicios/TarjetaInteractiva.jsx
+++ src/ejercicios/TarjetaInteractiva.jsx
@@ -5,17 +5,21 @@
 
 import { useState } from "react";
 
+//creando componente
 const TarjetaInteractiva = () => {
 
+    //creando variable de estado que funciona como bandera (flag)
     const [toggle, setToggle] = useState(false);
 
-
+    //Creando componente que renderiza la card
     const Tarjeta = () => {
+        //si toggle es true, muestra un contenido
         if(toggle){
             return(<p style={{color:"red"}}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vitae quaerat aperiam
             corrupti, beatae ad esse! Atque ea ab sapiente nemo aliquam assumenda, ut esse alias? 
             Nemo cupiditate numquam aspernatur ipsum!</p>)
         }else{
+            //si toggle es false, muestra otro contenido
             return( 
                 <p style={{color:"green"}}>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
                 aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
@@ -25,7 +29,8 @@ const TarjetaInteractiva = () => {
     return(<>
     <div>
         <h2>14. Tarjeta Interactiva </h2>
-        
+        {/* Renderiza la card y en el boton se le setea a la variable toggle, cada vez
+        que se clickee, el valor diferente a la que contenga toggle*/}
         <Tarjeta />
         <button onClick={() => { setToggle(!toggle)}}>Toggle</button>
 

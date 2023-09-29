c60c25c7d6e08a9526a9420ec5ad1ac941258324
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-09-29 19:43:52 -0300

    Agregando productos al array de objetos

--- src/ejercicios/ObjetosValor.jsx
+++ src/ejercicios/ObjetosValor.jsx
@@ -4,18 +4,70 @@
 //(por ejemplo, mostrar solo los elementos con un precio superior a $50).
 //con un input para determinar el precio a calcular
 
+import { useState } from "react";
 const ObjetosValor = () => {
 
+    const Objetos = [{
+        articulo: "Celular",
+        marca: "Samsung",
+        color: "Blanco",
+        precio: 250
+    },
+    {
+        articulo: "Lavarropas",
+        marca: "Philips",
+        color: "Blanco",
+        precio: 1500
+    },
+    {
+        articulo: "Celular",
+        marca: "Huawei",
+        color: "Negro",
+        precio: 150
+    },
+    {
+        articulo: "Celular",
+        marca: "Samsung",
+        color: "Negro",
+        precio: 750
+    },
+    {
+        articulo: "Auriculares",
+        marca: "Lenovo",
+        color: "Negros",
+        precio: 50
+    },
+    {
+        articulo: "Mouse",
+        marca: "Logitech",
+        color: "Gris",
+        precio: 170
+    },
+    {
+        articulo: "Teclado",
+        marca: "Aorus",
+        color: "Blanco",
+        precio: 180
+    },
+    {
+        articulo: "Microfono",
+        marca: "Aorus",
+        color: "Negro",
+        precio: 110
+    }
+    ]
 
 
-    return(<>
-    <div>
-        <h2>16. Objetos con un valor mayor a precio especifico</h2>
-    
-    
-    
-    </div>
-    
+    return (<>
+        <div>
+            <h2>16. Objetos con un valor mayor a precio especifico</h2>
+
+
+
+
+
+        </div>
+
     </>)
 
 }

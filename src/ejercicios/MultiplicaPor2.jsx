5452d752f268ad20191b881dacb5b5802f2fb708
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-09-30 00:28:26 -0300

    Ejercicio 17 documentado completo

--- src/ejercicios/MultiplicaPor2.jsx
+++ src/ejercicios/MultiplicaPor2.jsx
@@ -4,14 +4,21 @@
 
 import { useState } from "react";
 
+//creando componente
 const MultiplicaPor2 = () => {
 
+    //creando array inicial de numeros
     const arrayNumeros = [12,6,8,23,3,17];
 
+    //creando variable de estado que se inicializa con el array creado anteriormente
     const [resultado, setResultado] = useState(arrayNumeros);
 
+    //creando funcion que multiplica cada numero del array dependiendo el valor que se le pase 
+    //por parametro
     const multiplicarArray = (valor) => {
+        //guardando en una variable el resultado de los numeros multiplicados en el array
         const nuevoArray = resultado.map((num) => num * valor);
+        //seteando la variable de estado con el nuevo resultado
         setResultado(nuevoArray);
 
     }
@@ -19,10 +26,11 @@ const MultiplicaPor2 = () => {
     return(<>
     <div>
         <h2>17. Array multiplicado </h2>
-
+        {/* Cada boton se le pasa un parametro con un valor diferente*/}
         <button onClick={() => multiplicarArray(2)}>x2</button>{" "}
         <button onClick={() => multiplicarArray(5)}>x5</button>{" "}
         <button onClick={() => multiplicarArray(10)}>x10</button><br/><br/>
+        {/* Muestra el array */}
         <span>{`[${resultado}]`}</span>
 
 

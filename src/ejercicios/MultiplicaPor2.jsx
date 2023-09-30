48a08316b598a80e405189756048c2a10d1f478e
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-09-30 00:22:39 -0300

    Resolviendo ejercicio 17

--- src/ejercicios/MultiplicaPor2.jsx
+++ src/ejercicios/MultiplicaPor2.jsx
@@ -6,12 +6,19 @@ import { useState } from "react";
 
 const MultiplicaPor2 = () => {
 
+    const arrayNumeros = [12,6,8,23,3,17];
+
+    const [resultado, setResultado] = useState(arrayNumeros);
+
 
     return(<>
     <div>
         <h2>17. Array multiplicado </h2>
 
-
+        <button>x2</button>{" "}
+        <button>x5</button>{" "}
+        <button>x10</button><br/><br/>
+        <span>{`[${resultado}]`}</span>
 
 
     </div>

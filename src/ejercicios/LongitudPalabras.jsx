62fbac937a6433ba1a9413e5cb0ff779072cb0fd
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-10-04 18:44:24 -0300

    Agregando ejercicio 19

---
+++ src/ejercicios/LongitudPalabras.jsx
@@ -0,0 +1,30 @@
+
+
+//19. Mapeo de Palabras: Dado un array de palabras, utiliza la función map para crear 
+//un nuevo array que contenga la longitud de cada palabra.
+
+import { useState } from "react";
+
+
+const LongitudPalabras = () => {
+
+    const [arrayPalabras, setArrayPalabras] = useState("");
+
+
+
+    return(<>
+    <div>
+        <h2>19. Determina longitud de palabras </h2>
+
+        <input />
+
+
+    </div>
+
+    </>)
+
+
+}
+
+
+export default LongitudPalabras;

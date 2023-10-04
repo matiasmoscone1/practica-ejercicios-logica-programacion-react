a4313c6bf8d2d51e033f633acd27025966b4e01f
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-10-04 19:22:27 -0300

    Ejercicio 19 completamente terminado y documentado

--- src/ejercicios/LongitudPalabras.jsx
+++ src/ejercicios/LongitudPalabras.jsx
@@ -5,30 +5,37 @@
 
 import { useState } from "react";
 
-
+// creando componente 
 const LongitudPalabras = () => {
 
+    //creando variables de estado
     const [arrayPalabras, setArrayPalabras] = useState("");
     const [arrayLongitud, setArrayLongitud] = useState([]);
     
+    //creando funcion que recibe por parametro el texto escrito por el usuario
     const longitud = (texto) => {
+        //convirtiendo ese texto en un array de palabras
         const nuevoArray = texto.split(" ");
+        //crea un nuevo array con la longitud de cada palabra dentro del array de palabras
         const longitudes = nuevoArray.map((palabra) => palabra.length);
+        //setea nuevamente el arrayLongitud
         setArrayLongitud(longitudes);     
         //console.log(arrayLongitud);
     }
 
     //console.log(arrayPalabras.split(" "));
 
-    console.log(arrayLongitud);
+    //console.log(arrayLongitud);
 
     return(<>
     <div>
         <h2>19. Determina longitud de palabras en array</h2>
-
+        {/* textarea que es donde escribe el usuario el texto*/}
         <textarea cols={40} rows={6} placeholder="Escribe el texto" onChange={(e) => setArrayPalabras(e.target.value)}/>
         <br/>
+        {/* boton que llama a la funcion principal donde se realiza la logica del ejercicio*/}
         <button onClick={() => longitud(arrayPalabras)}>Calcular</button>
+        {/* Muestra el resultado en pantall */}
         <p>{`[${arrayLongitud.join(",")}]`}</p>
        
 

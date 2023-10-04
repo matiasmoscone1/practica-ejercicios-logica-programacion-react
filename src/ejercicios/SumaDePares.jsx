bee93af9576c7cf1764cbb84aa46d4917bfae2f1
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-10-04 19:33:36 -0300

    Maquetando ejercicio 20

--- src/ejercicios/SumaDePares.jsx
+++ src/ejercicios/SumaDePares.jsx
@@ -8,11 +8,17 @@ import { useState } from "react";
 const SumaDePares = () => {
 
 
+    const array = [20,13,6,23,2,7,15,12,9,5,4];
+
+
+
+
     return(<>
     <div>
         <h2>20. Suma de numeros pares dentro de un array</h2>
-
-
+        {`[${array.join(",")}]`}
+        <br/><br/>
+        <button>Calcular</button>
 
     </div>
     </>)

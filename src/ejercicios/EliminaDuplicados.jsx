54ac4dd45c71832dd64993f6892001c89b1a77ea
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-10-05 20:31:00 -0300

    Resolviendo ejercicio 20

--- src/ejercicios/EliminaDuplicados.jsx
+++ src/ejercicios/EliminaDuplicados.jsx
@@ -3,14 +3,24 @@
 //duplicados, devolviendo un nuevo array sin duplicados.
 
 
-
 const EliminaDuplicados = () => {
+
+    const array = [2,2,6,15,8,6,22,3,9,12,3,3];
+
+
+    const eliminaDuplicados = () => {
+        const resultado = array.filter((num) => num === num);
+        console.log(resultado);
+    }
+
 
 
     return(<>
     <div>
         <h2>21. Elimina duplicados de un array</h2>
-        
+        {`[${array.join(",")}]`}
+
+        <button onClick={() => eliminaDuplicados()}>Eliminar Duplicados</button>
 
     </div>
     </>)

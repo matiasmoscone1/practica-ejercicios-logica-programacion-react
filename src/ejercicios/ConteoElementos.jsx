2c40935a1e63e81cb5caef24039743d51a5d834a
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-10-06 17:55:27 -0300

    Agregando ejercicio 22

---
+++ src/ejercicios/ConteoElementos.jsx
@@ -0,0 +1,26 @@
+
+//22. Conteo de Elementos: Crea una función que cuente la cantidad de veces 
+//que aparece un elemento específico en un array.
+
+const ConteoElementos = () => {
+
+    const array = ["asd", 1, {nombre: "matias", apellido: "moscone"}, "asd", 34, false, 22,
+    "asd", "55", true, "holabuenosdias", "asd"];
+
+
+    return(<>
+    <div>
+        <h2>22. Conteo de elemento especifico </h2>
+    
+
+
+
+    
+    
+    </div>
+    </>)
+
+
+}
+
+export default ConteoElementos;

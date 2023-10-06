3f2607ddee87365af6a3c0e42ad9092fcb45e66d
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-10-06 17:35:54 -0300

    Ejercicio 21 resuelto

--- src/ejercicios/EliminaDuplicados.jsx
+++ src/ejercicios/EliminaDuplicados.jsx
@@ -2,10 +2,13 @@
 //21. Eliminar Duplicados: Escribe una función que tome un array y elimine los elementos 
 //duplicados, devolviendo un nuevo array sin duplicados.
 
+import { useState } from "react";
+
 
 const EliminaDuplicados = () => {
 
     const array = [2,2,6,15,8,6,22,3,9,12,3,3];
+    const [nuevoArray, setNuevoArray] = useState([]);
 
 
     const eliminaDuplicados = () => {
@@ -15,6 +18,7 @@ const EliminaDuplicados = () => {
             numeros.add(num);
         });
         console.log(numeros);
+        setNuevoArray(Array.from(numeros));
     }
 
 
@@ -23,8 +27,8 @@ const EliminaDuplicados = () => {
     <div>
         <h2>21. Elimina duplicados de un array</h2>
         {`[${array.join(",")}]`}
-
         <button onClick={() => eliminaDuplicados()}>Eliminar Duplicados</button>
+        {`[${nuevoArray.join(",")}]`}
 
     </div>
     </>)

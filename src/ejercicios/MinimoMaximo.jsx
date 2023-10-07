7e5c4ccc2ccb94a1732d5af5a4b16203ce54f8ba
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-10-07 20:27:33 -0300

    Resolviendo ejercicio 23

--- src/ejercicios/MinimoMaximo.jsx
+++ src/ejercicios/MinimoMaximo.jsx
@@ -2,15 +2,24 @@
 //23. Buscar Elemento Máximo/Mínimo: Escribe una función que encuentre el 
 //elemento máximo o mínimo en un array de números.
 
+import { useState } from "react";
+
 const MinimoMaximo = () => {
 
     const array = [2, 6, 47, 32, 53, 14, 22, 27, 3, 19];
 
+    const [resultado, setResultado] = useState(0);
+
+
+
     return(<>
     <div>
         <h2>23. Busca el minimo o maximo del array</h2>
-
         {`[${array.join(",")}]`}
+        <br/><br/>
+        <button>Maximo</button>{" "}
+        <button>Minimo</button>{" "}
+        {resultado}
 
     </div>
     </>)

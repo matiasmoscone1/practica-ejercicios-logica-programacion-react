845f0c78f9ed19c1500be0d1235fceab22607a81
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-10-09 19:34:01 -0300

    Ejercicio 25 documentado completo

--- src/ejercicios/ReduccionDeCadenas.jsx
+++ src/ejercicios/ReduccionDeCadenas.jsx
@@ -4,19 +4,25 @@
 
 import { useState } from "react";
 
-
+//creando componente 
 const ReduccionDeCadenas = () => {
-
+    //creando variables de estado, arrayCadenas es todo lo que ingresa el usuario en textarea
     const [arrayCadenas, setArrayCadenas] = useState(null);
+    //resultado es el array reducido final
     const [resultado, setResultado] = useState("");
 
-    console.log(arrayCadenas);
+    //console.log(arrayCadenas);
 
+    //creando funcion que reduce las cadenas a una sola
     const reduceCadenas = (arr) => {
+        //convierte las cadenas en un array de cadenas
         const nuevoArray = arr.split(" ");
         //console.log(nuevoArray);
 
+        //reduce a una sola cadena el array de cadenas, str es el acumulador y valor es el 
+        //valor actual que va "iterando"
         const arrayReducido = nuevoArray.reduce((str, valor) => str + valor);
+        //guarda el array reducido en la variable de estado resultado
         setResultado(arrayReducido);
         //console.log(arrayReducido);
 
@@ -26,9 +32,14 @@ const ReduccionDeCadenas = () => {
     return(<>
     <div>
         <h2>25. Reduce las cadenas a una sola </h2>
+        {/* textarea donde el usuario escribe y guarda el valor del textarea en la variable
+        de estado arrayCadenas */}
         <textarea cols={50} rows={6} onChange={(e) => {setArrayCadenas(e.target.value)}}/>
         <br/><br/>
+        {/* al evento onClick del boton "Reducir" se le pasa la funcion reductora con el parametro
+        de las cadenas escritas en el textarea */}
         <button onClick={() => reduceCadenas(arrayCadenas)}>Reducir</button><br/><br/>
+        {/* muestra el resultado en pantalla */}
         {`El array reducido es: ${resultado}`}
     </div>
     </>)

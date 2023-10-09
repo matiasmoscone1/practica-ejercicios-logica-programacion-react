03e59b6cb6d7722061726eadd3efce26a644e5d0
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-10-09 19:39:08 -0300

    Maquetando ejercicio 26

--- src/ejercicios/SumaReduce.jsx
+++ src/ejercicios/SumaReduce.jsx
@@ -2,15 +2,28 @@
 //26. Reduce para Sumar: Utiliza la función reduce para calcular la suma de todos los 
 //números en un array.
 
+import { useState } from "react";
+
 
 const SumaReduce = () => {
+
+    const arrayNumeros = [2, 5, 8, 10, 22, 30];
+
+    const [resultado, setResultado] = useState(0);
+
+    const sumaReduciendo = () => {
+
+
+    }
 
 
     return(<>
     <div>
         <h2>26. Suma de array utilizando reduce</h2>
-    
-    
+        {`[${arrayNumeros.join(",")}]`}
+        <br/><br/>
+        <button onClick={() => sumaReduciendo()}>Sumar Reduciendo</button>
+        {resultado ? "hola" : ""}
     </div>
     </>)
 

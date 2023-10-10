702187c0c81bccd08f78ba5c9f3237153ac3921c
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-10-10 19:41:46 -0300

    Documentando ejercicio 26

--- src/ejercicios/SumaReduce.jsx
+++ src/ejercicios/SumaReduce.jsx
@@ -4,15 +4,20 @@
 
 import { useState } from "react";
 
-
+//creando componente
 const SumaReduce = () => {
 
+    //creando array inicial de numeros
     const arrayNumeros = [2, 5, 8, 10, 22, 30];
 
+    //creando variable de estado resultado
     const [resultado, setResultado] = useState(0);
 
+    //creando funcion reductora, toma el array por parametro
     const sumaReduciendo = (arr) => {
+        //crea un nuevo array reducido con reduce, el valor actual es el b y a el acumulador
         const arrayReducido = arr.reduce((a, b) => a + b);
+        //setea en la variable de estado el nuevo array reducido
         setResultado(arrayReducido);
     }
 

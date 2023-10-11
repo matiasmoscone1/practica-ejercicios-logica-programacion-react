4e58686f63f823e8f990eab03fa08a9aab5b66ad
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-10-11 18:48:08 -0300

    Maquetando y resolviendo ejercicio 27

--- src/ejercicios/CuentaElementos.jsx
+++ src/ejercicios/CuentaElementos.jsx
@@ -3,10 +3,16 @@
 //27. Cuenta de elementos en un arreglo: Dado un arreglo, utiliza reduce para contar 
 //cuántas veces aparece un valor específico en el arreglo. (utilizando reduce)
 
+import { useState } from "react";
+
 
 const CuentaElementos = () => {
 
 
+    const array = ["hola", 3, 6, "hola", false, 70, true, "hola", 6, "6", "asdasd", "true",
+    true, true, 6, "hola", {asd: "asd", num: 2}, ["gg", "gg", "gg"]];
+
+    const [resultado, setResultado] = useState(null);
     
 
 

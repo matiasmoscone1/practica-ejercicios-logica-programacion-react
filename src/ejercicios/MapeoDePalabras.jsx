3084cc03feaccc3683f5701bbbb2b1cfa9386884
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-10-12 23:20:46 -0300

    Resolviendo y maquetando ejercicio 28

--- src/ejercicios/MapeoDePalabras.jsx
+++ src/ejercicios/MapeoDePalabras.jsx
@@ -1,11 +1,27 @@
 
 
-//28. Mapeo de Palabras: Dado un array de palabras, utiliza la función map para 
+//28. Mapeo de Palabras: Dado un array de palabras, utiliza la función reduce para 
 //crear un nuevo array que contenga la longitud de cada palabra.
+
+import { useState } from "react";
 
 const MapeoDePalabras = () => {
 
+    const [resultado, setResultado] = useState(null);
 
+
+
+    return(<>
+    <div>
+        <h2>28. Longitud de palabras de un array con reduce</h2>
+        <textarea cols={40} rows={6}/><br/><br/>
+        <button>Contar longitud</button>{" "}
+        {resultado}
+
+
+
+    </div>
+    </>)
 
 }
 

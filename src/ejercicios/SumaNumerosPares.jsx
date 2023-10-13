d6e4f0f81f0dbb56b20ad4d8716c806d3e48203b
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-10-13 20:13:59 -0300

    Maquetando ejercicio 28

--- src/ejercicios/SumaNumerosPares.jsx
+++ src/ejercicios/SumaNumerosPares.jsx
@@ -6,13 +6,23 @@
 
 const SumaNumerosPares = () => {
 
+    const arrayNumeros = [2, 54, 67, 33, 85, 62, 72, 11, 16, 99];
+
+
+    const cuentaPares = (arr) => {
+
+        
+
+    }
+
 
 
     return(<>
     <div>
         <h2>29. Suma los numeros pares de un array</h2>
+        {`[${arrayNumeros.join(",")}]`}
+        <button onClick={() => cuentaPares(arrayNumeros)}>Cuenta Pares</button>
 
-        
     </div>
     </>)
 

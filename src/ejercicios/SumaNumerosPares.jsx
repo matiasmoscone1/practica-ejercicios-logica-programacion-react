1b935f834af983f16a79fbb976255e98b1cc134b
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-10-14 16:18:26 -0300

    ejercicio 29 documentado completamente

--- src/ejercicios/SumaNumerosPares.jsx
+++ src/ejercicios/SumaNumerosPares.jsx
@@ -5,20 +5,26 @@
 
 import { useState } from "react";
 
-
+//creando componente
 const SumaNumerosPares = () => {
 
+    //creando array de numeros y variable de esatdo donde se almacenara el resultado
     const arrayNumeros = [2, 54, 67, 33, 85, 62, 72, 11, 16, 99];
     const [resultado, setResultado] = useState(null);
 
+    //creando funcion que se le pasa por parametro un array de numeros
     const cuentaPares = (arr) => {
 
-        const arrayReducido = arr.reduce((acc, val) => {
+        //se utiliza la funcion reduce con un operador ternario
+        arr.reduce((acc, val) => {
+            //el cual tiene un acumulador donde va acumulando si el numero es par
             val % 2 === 0 ? acc+=val : acc;
  
+            //setea el acumulador como resultado final
             setResultado(acc);
+            //devuelve el acumulador (podria omitirse el return en este caso)
             return acc;
-
+        //se pasa por segundo parametro de la funcion reduce el valor inicial
         }, 0);
         
 
@@ -31,9 +37,12 @@ const SumaNumerosPares = () => {
     return(<>
     <div>
         <h2>29. Suma los numeros pares de un array</h2>
+        {/* Muestra array de numeros en pantalla */}
         {`[${arrayNumeros.join(",")}]`}{" "}
+        {/* dos botones que pasan la funcion correspondiente para cada boton */}
         <button onClick={() => cuentaPares(arrayNumeros)}>Cuenta Pares</button>{" "}
         <button onClick={() => setResultado("")}>Limpiar</button>{" "}
+        {/* muestra el resultado en pantalla */}
         {resultado}
 
     </div>

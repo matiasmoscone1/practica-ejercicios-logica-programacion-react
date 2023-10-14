b3ab019cf1390c2fb1ebb869eb9c6a3860b211a6
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-10-14 16:14:39 -0300

    Ejercicio 29 terminado completamente, falta documentar

--- src/ejercicios/SumaNumerosPares.jsx
+++ src/ejercicios/SumaNumerosPares.jsx
@@ -3,25 +3,28 @@
 //29. Suma de Números Pares: Dado un array de números, utiliza funciones de programación 
 //funcional para calcular la suma de todos los números pares en el array.
 
+import { useState } from "react";
+
 
 const SumaNumerosPares = () => {
 
     const arrayNumeros = [2, 54, 67, 33, 85, 62, 72, 11, 16, 99];
-
+    const [resultado, setResultado] = useState(null);
 
     const cuentaPares = (arr) => {
 
         const arrayReducido = arr.reduce((acc, val) => {
             val % 2 === 0 ? acc+=val : acc;
  
+            setResultado(acc);
             return acc;
+
         }, 0);
         
-        console.log(arrayReducido);
 
-        }
+        //console.log(arrayReducido);
+    }
     
-  
 
 
 
@@ -29,7 +32,9 @@ const SumaNumerosPares = () => {
     <div>
         <h2>29. Suma los numeros pares de un array</h2>
         {`[${arrayNumeros.join(",")}]`}{" "}
-        <button onClick={() => cuentaPares(arrayNumeros)}>Cuenta Pares</button>
+        <button onClick={() => cuentaPares(arrayNumeros)}>Cuenta Pares</button>{" "}
+        <button onClick={() => setResultado("")}>Limpiar</button>{" "}
+        {resultado}
 
     </div>
     </>)

ba6991812eccf27563412542a7caa1f517f58c7b
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-10-14 16:49:15 -0300

    Ejercicio 30 terminado, completo, faltan detalles

--- src/ejercicios/DuplicadosReduce.jsx
+++ src/ejercicios/DuplicadosReduce.jsx
@@ -3,19 +3,35 @@
 //30. Eliminar Duplicados: Escribe una función que tome un array y elimine los elementos 
 //duplicados, devolviendo un nuevo array sin duplicados.
 
+import { useState } from "react";
+
 
 const DuplicadosReduce = () => {
 
     const arrayNumeros = [3, 5, 16, 3, 32, 18, 3, 5, 22, 2, 7, 16, 73, 32];
+    const [resultado, setResultado] = useState([]);
 
+    const eliminaReduce = (arr) => {
 
+        const nuevoArray = arr.reduce((list, val) => {
+            if(!list.includes(val)){
+                list.push(val);
+            }
+            return list;
+        }, []);
+
+        setResultado(nuevoArray);
+    }
+
+    
 
     return(<>
     <div>
         <h2>30. Elimina duplicados con funcion reduce</h2>
 
-        {`[${arrayNumeros.join(",")}]`}
-
+        {`[${arrayNumeros.join(",")}]`}{" "}
+        <button onClick={() => eliminaReduce(arrayNumeros)}>Eliminar</button>
+        {`[${resultado}]`}
 
 
     </div>

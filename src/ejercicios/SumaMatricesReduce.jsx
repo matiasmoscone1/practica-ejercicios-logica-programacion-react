d12950da0b6adbe482d1dc07892b4a0588b78925
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-10-18 16:28:14 -0300

    Terminando de documentar ejercicio 32

--- src/ejercicios/SumaMatricesReduce.jsx
+++ src/ejercicios/SumaMatricesReduce.jsx
@@ -22,23 +22,31 @@ const SumaMatricesReduce = () => {
             const nuevoArray = [];
             //si el largo del arreglo1 es mayor q el 2do, se toma como referencia el arr1
             //y se le hace la funcion reduce desde ese arreglo.        
-            if(arr1.length > arr2.length){                
+            if(arr1.length > arr2.length){   
+                //aplica funcion reduce con 3 parametros, primer parametro es la suma de los
+                //valores, segundo el valor y tercero un iterador              
                 arr1.reduce((arrSum, val, i = 0) => {
                     arrSum = (val || 0) + (arr2[i] || 0);
                     i++;          
                     //console.log(arrSum);
+                    //pone el valor sumado en un nuevo array vacio
                     nuevoArray.push(arrSum);
                 }, 0);
             //si el largo del arreglo2 es mayor o igual q el 1ero, se toma como referencia 
             //el arr2 y se le hace la funcion reduce desde ese arreglo.
             }else{                
+                
+                //aplica funcion reduce con 3 parametros, primer parametro es la suma de los
+                //valores, segundo el valor y tercero un iterador
                 arr2.reduce((arrSum, val, i = 0) => {
                     arrSum = (val || 0) + (arr1[i] || 0);
                     i++;          
                     //console.log(arrSum);
+                    //pone el valor sumado en un nuevo array vacio
                     nuevoArray.push(arrSum);
                 }, 0);
             }
+            //setea el resultado en la variable de estado
         setResultado(nuevoArray);
     }
 
@@ -46,10 +54,13 @@ const SumaMatricesReduce = () => {
     return(<>
     <div>
         <h2>32. Suma de matrices con reduce </h2>
+        {/* muestra los dos arrays */}
         {`[${array1.join(",")}]`} - {`[${array2.join(",")}]`}<br/><br/>
 
+        {/* crea dos botones y le pasa sus respectivas funciones con parametros */}
         <button onClick={() => sumarMatrices(array1, array2)}>Sumar Matrices</button>{" "}
         <button onClick={() => setResultado(null)}>Limpiar</button>
+        {/* si existe resultado, lo muestra en pantalla, sino muestra cadena vacia */}
         <p>{resultado ? `Resultado: [${resultado}]` : " "}</p>
     </div>
     </>)

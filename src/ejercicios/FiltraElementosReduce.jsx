e20f795564e3b9baa42b40e8bbf68b4afe37420b
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-10-19 19:31:20 -0300

    Resolviendo logica del ej 33

--- src/ejercicios/FiltraElementosReduce.jsx
+++ src/ejercicios/FiltraElementosReduce.jsx
@@ -2,14 +2,23 @@
 //33. Filtrado de elementos: Dado un arreglo de objetos, utiliza reduce para filtrar 
 //los elementos que cumplan cierta condición y crear un nuevo arreglo con esos elementos.
 
+import { useState } from "react";
+
 
 const FiltraElementosReduce = () => {
 
     const arrayNumeros = [2, 33, 15, 32, 64, 9, 12, 24, 7, 5, 19, 29, 22, 4, 43, 18, 1];
 
+    const [resultado, setResultado] = useState(null);
 
     const filtraPares = (arr) => {
-
+        const nuevoArray = [];
+        arr.reduce((acc, val) => {
+            if(val % 2 === 0){
+                acc = nuevoArray.push(val);
+            }
+        }, 0);
+        setResultado(nuevoArray);
     }
 
     const filtraImpares = (arr) => {
@@ -22,12 +31,14 @@ const FiltraElementosReduce = () => {
 
     }
 
+    console.log(resultado);
+
 
     return(<>
     <div>
         <h2>33. Filtrado de elementos con reduce</h2>
         <p>{`[${arrayNumeros.join(",")}]`}</p>
-        <button>Nros Pares</button>{" "}
+        <button onClick={() => filtraPares(arrayNumeros)}>Nros Pares</button>{" "}
         <button>Nros Impares</button>{" "}
         <button>Nros Primos</button>{" "}
         <button>Multiplo de 3</button>

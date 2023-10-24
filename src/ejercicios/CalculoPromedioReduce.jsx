41707bbec169e6dad8d227f3817222953d094ffe
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-10-24 16:17:28 -0300

    Maquetando ejercicio 34

--- src/ejercicios/CalculoPromedioReduce.jsx
+++ src/ejercicios/CalculoPromedioReduce.jsx
@@ -2,17 +2,21 @@
 
 //34. Calcular el promedio: Dado un arreglo de números, utiliza reduce para calcular 
 //el promedio de los valores en el arreglo.
-
+import { useState } from "react";
 
 const CalcularPromedioReduce = () => {
 
+    const arrayNumeros = [3, 6, 15 ,10, 16, 21, 33, 27, 28];
 
+    const [resultado, setResultado] = useState(null);
+    
     return(<>
     <div>
         <h2>34. Calcula el promedio de un array con reduce</h2>
-
-
-        
+        <p>{`[${arrayNumeros.join(",")}]`}</p>
+        <button>Calcular</button>
+        <button>Limpiar</button>
+        <p>{`Resultado: ${resultado}`}</p>
     </div>
     </>)
 

8d635b327253d41e14f3787ab4b112bbd732a637
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-11-11 14:23:05 -0300

    Terminando logica defunciones del ejercicio 36

--- src/ejercicios/FlattenArreglo.jsx
+++ src/ejercicios/FlattenArreglo.jsx
@@ -3,34 +3,37 @@
 //36. Flatten de arreglos anidados: Dado un arreglo de arreglos, utiliza reduce para 
 //aplanar la estructura y obtener un único arreglo con todos los elementos.
 
+import { useState } from "react";
+
 const FlattenDeArreglos = () => {
 
     const arrayPrincipal = [[1,6,3,9,14], ["hola", "como", "andas"], [1, true, "hola", false],
     [{nombre: "matias", apellido: "moscone", edad: 27}], ["hola", 54, {objeto: "esto es un objeto"}]];
 
+    const [resultado, setResultado] = useState(null);
 
     const reducirArreglo = (arr) => {
         const nuevoArray = arr.reduce((acc, val) => {
             acc.push(val);
             return(acc);
         }, []);
-        return nuevoArray;
+        setResultado(nuevoArray);
         //console.log(nuevoArray);
     }
 
     //console.log(arrayPrincipal);
 
-    const juntarArreglos = () => {
-        reducirArreglo(arrayPrincipal);
-    }
+    
+
+
     return(<>
     <div>
         <h2>36. Flatten de arreglos anidados con reduce</h2>
 
         {`[${arrayPrincipal.join("][")}]`}
-        <button onClick={() => juntarArreglos()}>Añidar</button> 
-
-
+        <button onClick={() => reducirArreglo(arrayPrincipal)}>Añidar</button> 
+        <button onClick={() => limpiar()}>Limpiar</button>
+        {resultado ? resultado : ""}
     </div>
     </>)
 

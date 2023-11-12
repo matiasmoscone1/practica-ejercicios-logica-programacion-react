c8cfd94486322cfdfd572a68967d6cba3b0ba03d
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-11-12 17:38:19 -0300

    Ejercicio 36 completamente documentado

--- src/ejercicios/FlattenArreglo.jsx
+++ src/ejercicios/FlattenArreglo.jsx
@@ -15,33 +15,35 @@ const FlattenDeArreglos = () => {
     //creando variable de estado que almacenara el resultado
     const [resultado, setResultado] = useState(null);
 
-    //console.log(arrayPrincipal);
-    
     //creando funcion reductora de array que contendra toda la logica, se pasa por parametro un array
     const redArray = (arr) => {
-        
+        //creando array donde se almacenara todo el resultado, se le hace un reduce al array
+        //pasado por parametro
         const nuevoArreglo = arr.reduce((acc, val) => {
+                //por cada valor que se itere en el array, se le hace una serie de verificaciones
                 val.map((valor) => {
+                    //si es un objeto, se extraen todos los valores del objeto
                     if(typeof valor === "object"){
                         const obj = Object.values(valor);
                         //console.log(obj);
+                        //se genera el array de valores del objeto y se itera el array, se añaden
+                        //cada valor del objeto al array acc
                         obj.map((i) => acc.push(i));
                     }
+                    //si no es un objeto, se almacena directamente el valor en el array acc
                     if(typeof valor !== "object"){
                         acc.push(valor);
                     }                    
                 })
+            //devuelve el array acc con todos los valores
             return acc;
+        //se inicializa acc como array vacio
         }, []);
+        //se guarda el nuevo array en la variable de estado resultado
         setResultado(nuevoArreglo);
     }
 
-
-
-    //console.log(arrayPrincipal);
-
-    console.log(resultado);
-
+    //se crea funcion limpiar para limpiar el resultado de la pantalla
     const limpiar = () => {
         setResultado(null);
     }
@@ -50,10 +52,14 @@ const FlattenDeArreglos = () => {
     <div>
         <h2>36. Flatten de arreglos anidados con reduce</h2>
 
+        {/* Se muestra el array principal con todos los valores */}
         {`[${arrayPrincipal.join("][")}]`}<br/><br/>
+        {/* Botones con distintas funcionalidades cada uno, se llama a la funcion especifica */}
         <button onClick={() => redArray(arrayPrincipal)}>Añidar</button>{" "}
         <button onClick={() => limpiar()}>Limpiar</button>
         <br/><br/>
+        {/* Muestra el resultado en forma de array general separando cada valor por coma (,) y si
+        no existe "resultado", no se muestra nada*/}
         {resultado ? `[${resultado.join(",")}]` : ""}
     </div>
     </>)

ba77b2dae80f4bd64262b2913d238673f0fdb7dd
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-11-13 17:25:59 -0300

    Resolviendo ejercicio 37

--- src/ejercicios/ComponenteFuncional.jsx
+++ src/ejercicios/ComponenteFuncional.jsx
@@ -2,17 +2,27 @@
 //37. Componentes Funcionales: Crea un componente funcional de React que reciba un 
 //array de números como propiedades y renderice la suma de esos números.
 
+import { useState } from "react";
+
 
 const ComponenteFuncional = (arrayNum) => {
 
     console.log(arrayNum);
 
+    const [resultado, setResultado] = useState(null);
+
+    const sumaArray = (arr) => {
+        return arr.reduce((acc, val) => acc += val);
+    }
+
+
     return(<>
     <div>
         <h2>37. Componente funcional con props</h2>
-
-
-
+        {arrayNum}
+        <button>Calcular</button>
+        <button>Limpiar</button>
+        {}
     </div>
     </>)
 

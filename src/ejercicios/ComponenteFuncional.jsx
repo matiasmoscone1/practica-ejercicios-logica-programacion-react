1389b56f0e6224b35e60b9cec9b76e6aa02c11c4
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-11-14 17:42:34 -0300

    Ejercicio 37 documentado completo

--- src/ejercicios/ComponenteFuncional.jsx
+++ src/ejercicios/ComponenteFuncional.jsx
@@ -7,29 +7,36 @@ import { useState } from "react";
 
 const ComponenteFuncional = (arrayNum) => {
 
-    //console.log(arrayNum);
-
+    //creando variable de estado donde almacenara el resultado
     const [resultado, setResultado] = useState(null);
 
+    //creando funcion que suma los componentes del arreglo
     const sumaArray = (arreglo) => {
-        console.log(arreglo);
+        //inicializa acumulador en 0
         let acc = 0;
+        //recorre el arreglo y va sumando cada valor en la variable acumulador
         arreglo.arr.map((val) => acc += val);        
+        //almacena el resultado en la variable de estado creada anteriormente
         setResultado(acc);
     }
 
-    //console.log(resultado);
+   
 
+    //crea funcion que limpia el resultado
     const limpiaResultado = () => {
+        //setea resultado a null
         setResultado(null);
     }
 
     return(<>
     <div>
         <h2>37. Componente funcional con props</h2>
+        {/* Muestra el array de numeros principal pasado como prop */}
         {`[${arrayNum.arr.join(",")}]`}<br/><br/>
+        {/* Botones que llaman a sus respectivas funciones */}
         <button onClick={() => sumaArray(arrayNum)}>Calcular</button>{" "}
         <button onClick={() => limpiaResultado()}>Limpiar</button><br/><br/>
+        {/* si existe resultado, lo muestra en pantala, sino no muestra nada */}
         {resultado ? `El resultado es: ${resultado}` : ""}
     </div>
     </>)

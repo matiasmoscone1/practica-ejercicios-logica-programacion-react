e7abefe3f171d84ede0c66d5279ab4a98b2d7957
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-11-17 17:31:56 -0300

    Maquetando ejercicio 39

--- src/ejercicios/ComponenteFormulario.jsx
+++ src/ejercicios/ComponenteFormulario.jsx
@@ -4,22 +4,37 @@
 //en componentes funcionales separados para el nombre de usuario, la dirección de correo 
 //electrónico y la contraseña. Estos componentes deben ser componibles y reutilizables.
 
+const CompNombre = () => {
+
+}
+const CompApellido = () => {
+    
+}
+const CompEmail = () => {
+    
+}
+const CompTelefono = () => {
+    
+}
+
+
 
 const ComponenteFormulario = () => {
-
-
-
 
     const previeneSubmit = (e) => {
         e.preventDefault();
     }
 
+
+
     return(<>
     <div>
         <h2>39. Formulario con composicion de componentes funcionales</h2>
         <form onSubmit={(e) => {previeneSubmit(e)}}>
-
-
+        <CompNombre />
+        <CompApellido />
+        <CompEmail />
+        <CompTelefono />
         </form>
     </div>
     </>)

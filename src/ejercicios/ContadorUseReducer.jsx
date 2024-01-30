909d55e69a00a1b1a852a106a49072a9d76b7fb5
matiasmoscone1 <matiasmoscone1@gmail.com>
2024-01-30 16:27:30 -0300

    Resolviendo ejercicio 40

--- src/ejercicios/ContadorUseReducer.jsx
+++ src/ejercicios/ContadorUseReducer.jsx
@@ -1,16 +1,24 @@
-
+import { useReducer } from "react";
 
 //40. Implementar un Contador con useReducer: Crea un contador que puede incrementar y 
 //decrementar el valor utilizando el hook useReducer. Muestra el valor actual del 
 //contador en el componente.
 
-
 const ContadorUseReducer = () => {
 
+
+    const initialState = {contador: 0};
     
+
+    const [state, dispatch] = useReducer(reducer, initialState)
+    
+
+
     return(<>
     <div>
         <h2>40. Contador con useReducer (hook useReducer)</h2>
+
+
 
     </div>
     </>)

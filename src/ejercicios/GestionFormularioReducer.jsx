e29da6524ddde24b79373fd389b4bdaefb4a9ac0
matiasmoscone1 <matiasmoscone1@gmail.com>
2024-02-14 08:11:30 -0300

    Ejercicio 43 terminado, faltan detalles, mostrar validacion en los input de form de forma instantea

--- src/ejercicios/GestionFormularioReducer.jsx
+++ src/ejercicios/GestionFormularioReducer.jsx
@@ -4,7 +4,6 @@
 //nombre, email, contraseña). Usa useReducer para gestionar el estado del formulario.
 //Implementa la capacidad de manejar cambios en los campos y mostrar el estado del formulario.
 
-import { useState } from "react";
 import { useReducer } from "react";
 
 
@@ -24,13 +23,7 @@ const GestionFormularioReducer = () => {
 
     const [state, dispatch] = useReducer(reducer, initialState);
 
-    const [validacion, setValidacion] = useState({
-        nombre: "",
-        apellido: "",
-        email: "",
-        telefono: ""
-    });
-
+    
     const cambiaValor = (valor) => {dispatch({type: "CAMBIA", value: valor})} 
 
     const validaForm = (campo, valorInput) => {
@@ -40,23 +33,13 @@ const GestionFormularioReducer = () => {
 
         switch(campo){
             case "nombre":
-                //regExNombreApellido.test(valorInput.valor) ? console.log("Pasa!!!") : console.log("NO PASA...");
-                regExNombreApellido.test(valorInput.valor) ? setValidacion(...validacion) : setValidacion("Debe tener entre 8 y 20 caracteres...");
-                setValidacion((prevState) => ({
-                    ...prevState,
-                    nombre: regExNombreApellido.test(valorInput) ? "" : "Debe tener entre 8 y 20 caracteres..."
-                }));
-                break;
-            
+                regExNombreApellido.test(valorInput.valor) ? console.log("Pasa!!!") : console.log("NO PASA...");
             case "apellido":
-                //regExNombreApellido.test(valorInput.valor) ? console.log("Pasa!!!") : console.log("NO PASA...");
-                regExNombreApellido.test(valorInput.valor) ? setValidacion("") : setValidacion("Debe tener entre 8 y 20 caracteres...");
+                regExNombreApellido.test(valorInput.valor) ? console.log("Pasa!!!") : console.log("NO PASA...");
             case "email":
-                //regExEmail.test(valorInput.valor) ? console.log("Pasa!!!") : console.log("NO PASA...");
-                regExEmail.test(valorInput.valor) ? setValidacion("") : setValidacion("Debe tener formato xxxx@xxx.xxx...");             
+                regExEmail.test(valorInput.valor) ? console.log("Pasa!!!") : console.log("NO PASA...");
             case "telefono":
-                //regExTelefono.test(valorInput.valor) ? console.log("Pasa!!!") : console.log("NO PASA...");
-                regExTelefono.test(valorInput.valor) ? setValidacion("") : setValidacion("Debe tener 10 numeros...");
+                regExTelefono.test(valorInput.valor) ? console.log("Pasa!!!") : console.log("NO PASA...");
             }      
     }   
     
@@ -75,13 +58,13 @@ const GestionFormularioReducer = () => {
         
         <form action="submit">
             <label>Nombre </label>
-            <input data="nombre" type="text" onChange={(e) => {cambiaValor(e.target.value); validaForm("nombre", state);}}/> {validacion.nombre && <span>{validacion.nombre}</span>}<br/><br/>
+            <input data="nombre" type="text" onChange={(e) => {cambiaValor(e.target.value); validaForm("nombre", state);}}/> <br/><br/>
             <label>Apellido </label>
-            <input data="apellido" type="text" onChange={(e) => {cambiaValor(e.target.value); validaForm("apellido", state);}}/>{validacion}<br/><br/>
+            <input data="apellido" type="text" onChange={(e) => {cambiaValor(e.target.value); validaForm("apellido", state);}}/><br/><br/>
             <label>Email </label>
-            <input data="email" type="text" onChange={(e) => {cambiaValor(e.target.value); validaForm("email", state);}}/>{validacion}<br/><br/>
+            <input data="email" type="text" onChange={(e) => {cambiaValor(e.target.value); validaForm("email", state);}}/><br/><br/>
             <label>Numero </label>
-            <input data="telefono" type="number" onChange={(e) => {cambiaValor(e.target.value); validaForm("telefono", state);}}/>{validacion}<br/><br/>
+            <input data="telefono" type="number" onChange={(e) => {cambiaValor(e.target.value); validaForm("telefono", state);}}/><br/><br/>
             <button onClick={(e) => {submitButton(e)}}>Enviar</button>
         </form>
 

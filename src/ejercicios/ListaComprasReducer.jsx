90f12983627681b0578a76390bc04de4f8352a4c
matiasmoscone1 <matiasmoscone1@gmail.com>
2024-02-22 10:36:34 -0300

    Terminando ejercicio 44

--- src/ejercicios/ListaComprasReducer.jsx
+++ src/ejercicios/ListaComprasReducer.jsx
@@ -72,9 +72,15 @@ const ListaComprasReducer = () => {
                 const index = state.lista.findIndex((art) => art.articulo === action.valor.articulo);
 
                 if(index !== -1){
+                    console.log(index);
+                    if(action.valor.cantidad === 1){
+                        return state.lista.filter((art) => art.articulo !== action.valor.articulo);
+                    }
                     const nuevaLista = [...state.lista];
                     nuevaLista[index].cantidad -= 1;
                     return {lista: nuevaLista}
+                }else{
+                    return state;
                 }
                 
               

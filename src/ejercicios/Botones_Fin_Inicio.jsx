f6024d07508c4b01b577d909b814701169bf9eae
matiasmoscone1 <matiasmoscone1@gmail.com>
2024-02-24 04:59:40 -0300

    Añadiendo funcionalidad al boton fin

--- src/ejercicios/Botones_Fin_Inicio.jsx
+++ src/ejercicios/Botones_Fin_Inicio.jsx
@@ -4,11 +4,19 @@
 const Botones_Fin_Inicio = () => {
 
 
+    const fin = () => {
+        return(window.scrollTo({
+            top: document.body.scrollHeight,
+            behavior: "smooth"
+        }));        
+    }
+
+
 
     return(<>
         <div className="btn-container">
             <button className="btn-inicio">Inicio</button>
-            <button className="btn-fin">Fin</button>
+            <button className="btn-fin" onClick={() => fin()}>Fin</button>
         </div>
     </>)
 

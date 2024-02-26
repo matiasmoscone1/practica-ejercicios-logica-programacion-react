d4e219b72c070b1efbee43f8851a6f98d4f435f7
matiasmoscone1 <matiasmoscone1@gmail.com>
2024-02-26 01:23:17 -0300

    Terminando barra de navegacion

--- src/ejercicios/Navbar.jsx
+++ src/ejercicios/Navbar.jsx
@@ -7,17 +7,52 @@ const Navbar = () => {
     const [flag, setFlag] = useState(false);
 
     const DespliegaNavbar = () => {
+
+        
+        const LlamaEjercicio = () => {
+            let buttons = [];
+            for(let i = 1; i <= 15; i++){
+                buttons.push(
+                <button key={i} onClick={() => apuntaEjercicio(i)}>Ejercicio {i}</button>
+                );
+            }     
+            
+            return buttons; 
+        }
+        /*
+        const LlamaEjercicio = () => {
+            
+            let buttons = "";
+            for(let i = 1; i < 15; i++){
+                buttons += <button onClick={() => apuntaEjercicio(i)}>Ejercicio {i}</button>;
+                console.log(buttons);
+            }
+            
+            return buttons;            
+        } */
+
+
         console.log("desplegando navbar...");
         return(
         <div className="btn-despliega-ejercicios-container">
-            <button>Ejercicio 1</button>
-            <button>Ejercicio 2</button>
-            <button>Ejercicio 3</button>
-            <button>Ejercicio 4</button>
-            <button>Ejercicio 5</button>
-            <button>Ejercicio 6</button>
+            {/* 
+            <button onClick={() => apuntaEjercicio(1)}>Ejercicio 1</button>
+            <button onClick={() => apuntaEjercicio(2)}>Ejercicio 2</button>
+            <button onClick={() => apuntaEjercicio(3)}>Ejercicio 3</button>
+            <button onClick={() => apuntaEjercicio(4)}>Ejercicio 4</button>
+            <button onClick={() => apuntaEjercicio(5)}>Ejercicio 5</button>
+            <button onClick={() => apuntaEjercicio(6)}>Ejercicio 6</button>
+            */}
+
+            <LlamaEjercicio />
+
+
 
         </div>);
+    }
+
+    const apuntaEjercicio = (n) => {
+        console.log(n);
     }
 
 

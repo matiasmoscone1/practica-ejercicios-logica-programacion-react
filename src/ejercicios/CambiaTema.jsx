e855a9bcae663fb8aaa27e617cfabbb50e32747c
matiasmoscone1 <matiasmoscone1@gmail.com>
2024-05-23 10:50:37 -0300

    Practicando useContext

--- src/ejercicios/CambiaTema.jsx
+++ src/ejercicios/CambiaTema.jsx
@@ -1,9 +1,23 @@
+import { useState } from "react";
 
 const CambiaTema = () => {
 
     const themeContext = React.createContext();
 
+    const [estado, setEstado] = useState(false);
 
+    const cambiaColor = () => {
+
+
+    }
+    
+    return(<>
+    
+        <themeContext.Provider>
+            <button className="btn-context"></button>
+        </themeContext.Provider>
+
+    </>)
 
 
 }

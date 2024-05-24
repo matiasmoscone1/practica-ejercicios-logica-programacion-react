26be601cd7a8f6d102c86d496bfe5a5a817a848b
matiasmoscone1 <matiasmoscone1@gmail.com>
2024-05-24 09:04:28 -0300

    Mejorando useContext

--- src/ejercicios/CambiaTema.jsx
+++ src/ejercicios/CambiaTema.jsx
@@ -6,7 +6,16 @@ const CambiaTema = () => {
 
     const tema = useContext(themeContext);
 
-    
+    if(tema){
+      document.body.style.backgroundColor = "#000000";
+      document.body.style.color = "#FFFFFF";
+      
+    }else{
+      document.body.style.backgroundColor = "#FFFFFF";
+      document.body.style.color = "#000000";
+    }
+
+
     return(<>
         <div>
           {tema && <h1>ESTOY PROBANDO USECONTEXT</h1>}

f78f193510d8eed6eeb9a93b474aa36352007999
matiasmoscone1 <matiasmoscone1@gmail.com>
2024-06-05 09:02:56 -0300

    Practicanado CRUD

--- src/ejercicios/EjercicioCRUD.jsx
+++ src/ejercicios/EjercicioCRUD.jsx
@@ -4,26 +4,31 @@
 const EjercicioCRUD = () => {
 
     const productos = [{
+        id: 1,
         articulo: "Celular",
         color: "Negro",
         precio: 230
     },
     {
+        id: 2,
         articulo: "Computadora",
         color: "Blanca",
         precio: 1030
     },
     {
+        id: 3,
         articulo: "Tablet",
         color: "Gris",
         precio: 330
     },
     {
+        id: 4,
         articulo: "Celular",
         color: "Blanco",
         precio: 370
     },
     {
+        id: 5,
         articulo: "Monitor",
         color: "Negro",
         precio: 120
@@ -36,7 +41,28 @@ const EjercicioCRUD = () => {
 
     return(
     <div>
-    
+        <table border={1}>
+            <thead>
+                <tr>
+                    <td>Id</td>
+                    <td>Articulo</td>
+                    <td>Color</td>
+                    <td>Precio</td>
+                </tr>
+            </thead>
+            <tbody>
+                {productos.map((producto) => {
+                    return(
+                        <tr key={producto.id}>
+                            <td>{producto.id}</td>                            
+                            <td>{producto.articulo}</td>
+                            <td>{producto.color}</td>
+                            <td>{producto.precio}</td>
+                        </tr>
+                    )
+                })}
+            </tbody>
+        </table>
     
     </div>);
 
